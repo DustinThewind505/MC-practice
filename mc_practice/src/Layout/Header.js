1e4462fa2d7275afc19697f8493b2b5ff9a6a7f1
unknown <Dustin.Guillen14@yahoo.com>
2021-01-24 21:58:45 -0700

    FUNCTIONALITY navbard subnavs rendered

--- mc_practice/src/Layout/Header.js
+++ mc_practice/src/Layout/Header.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 import logo from '../logo.svg';
 
 
 function HeaderComponent() {
+    const { pathname } = useLocation();
 
 
 
-
+console.log(pathname)
     return (
         <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
             <div>
                 <ul>
-                    <li><Link to='/GroceryClass'>Grocery List Class Component</Link></li>
-                    <li><Link to='GroceryFunctional'>Grocery List Functional Component</Link></li>
-                    <li><Link to='NotesClass'>Notes Class Component</Link></li>
-                    <li><Link to='NotesFunctional'>Notes Functional Component</Link></li>
+                    <li><NavLink to='/GroceryClass'>Grocery List Class Component</NavLink></li>
+                    <li><NavLink to='GroceryFunctional'>Grocery List Functional Component</NavLink></li>
+                    <li><NavLink to='NotesClass'>Notes Class Component</NavLink></li>
+                    {pathname === '/NotesClass' ? <li><NavLink to='GroceryFunctional'>☰ Grocery List Functional Component</NavLink></li> : null}
+                    <li><NavLink to='NotesFunctional'>Notes Functional Component</NavLink></li>
+                    {pathname === '/NotesFunctional' ? <li><NavLink to='GroceryClass'>☰ Grocery List Class Component</NavLink></li> : null}
                 </ul>
             </div>
             <img src={logo} className="App-logo" alt="logo" />
